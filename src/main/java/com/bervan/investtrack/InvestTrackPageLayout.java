3378b2771d5add28e21c4bd31874d67ef40fe517
berluk12@gmail.com <berluk12@gmail.com>
2025-10-08 00:07:57 +0200

    Update wallet menu button to use AbstractWalletView route
    - Fixed incorrect route used for wallet-specific menu button.
    - Added import for AbstractWalletView.

--- src/main/java/com/bervan/investtrack/InvestTrackPageLayout.java
+++ src/main/java/com/bervan/investtrack/InvestTrackPageLayout.java
@@ -3,6 +3,7 @@ package com.bervan.investtrack;
 import com.bervan.common.MenuNavigationComponent;
 import com.bervan.investtrack.view.AbstractStockPriceAlertsView;
 import com.bervan.investtrack.view.AbstractWalletBalanceView;
+import com.bervan.investtrack.view.AbstractWalletView;
 import com.bervan.investtrack.view.AbstractWalletsView;
 import com.vaadin.flow.component.icon.VaadinIcon;
 
@@ -15,7 +16,7 @@ public class InvestTrackPageLayout extends MenuNavigationComponent {
         addButtonIfVisible(menuButtonsRow, AbstractStockPriceAlertsView.ROUTE_NAME, "Alerts", VaadinIcon.ALARM.create());
         addButtonIfVisible(menuButtonsRow, AbstractWalletsView.ROUTE_NAME, "Wallets", VaadinIcon.WALLET.create());
         if (walletName != null && !walletName.isEmpty()) {
-            addButtonIfVisible(menuButtonsRow, AbstractWalletsView.ROUTE_NAME, walletName, VaadinIcon.INFO_CIRCLE.create());
+            addButtonIfVisible(menuButtonsRow, AbstractWalletView.ROUTE_NAME, walletName, VaadinIcon.INFO_CIRCLE.create());
         }
         add(menuButtonsRow);
     }
