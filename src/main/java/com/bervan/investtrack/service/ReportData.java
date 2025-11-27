fa4b11dfa8c8cb9d9d52c82be27dea516b747281
berluk12@gmail.com <berluk12@gmail.com>
2025-11-27 12:03:14 +0100

    Initialize default values for `ReportData` fields
    - Set default instances for lists and maps to prevent `NullPointerException`.
    - Initialized `BigDecimal` fields with a default value of 0 for consistency.

--- src/main/java/com/bervan/investtrack/service/ReportData.java
+++ src/main/java/com/bervan/investtrack/service/ReportData.java
@@ -5,24 +5,26 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Getter
 @Setter
 public class ReportData {
-    private List<StockPriceData> bestToInvest;
-    private List<StockPriceData> goodToInvest;
-    private List<StockPriceData> riskyToInvest;
-    private List<StockPriceData> goodInvestmentsBasedOnBestRecommendation;
-    private List<StockPriceData> goodInvestmentsBasedOnGoodRecommendation;
-    private List<StockPriceData> goodInvestmentsBasedOnRiskyRecommendation;
-    private List<StockPriceData> badInvestmentsBasedOnBestRecommendation;
-    private List<StockPriceData> badInvestmentsBasedOnGoodRecommendation;
-    private List<StockPriceData> badInvestmentsBasedOnRiskyRecommendation;
-    private BigDecimal goodInvestmentProbabilityBasedOnBestToday;
-    private BigDecimal goodInvestmentProbabilityBasedOnGoodToday;
-    private BigDecimal goodInvestmentProbabilityBasedOnRiskyToday;
-    private BigDecimal goodInvestmentTotalProbabilityBasedOnToday;
-    private Map<String, StockPriceData> morningMap;
+    private List<StockPriceData> bestToInvest = new ArrayList<>();
+    private List<StockPriceData> goodToInvest = new ArrayList<>();
+    private List<StockPriceData> riskyToInvest = new ArrayList<>();
+    private List<StockPriceData> goodInvestmentsBasedOnBestRecommendation = new ArrayList<>();
+    private List<StockPriceData> goodInvestmentsBasedOnGoodRecommendation = new ArrayList<>();
+    private List<StockPriceData> goodInvestmentsBasedOnRiskyRecommendation = new ArrayList<>();
+    private List<StockPriceData> badInvestmentsBasedOnBestRecommendation = new ArrayList<>();
+    private List<StockPriceData> badInvestmentsBasedOnGoodRecommendation = new ArrayList<>();
+    private List<StockPriceData> badInvestmentsBasedOnRiskyRecommendation = new ArrayList<>();
+    private BigDecimal goodInvestmentProbabilityBasedOnBestToday = BigDecimal.valueOf(0);
+    private BigDecimal goodInvestmentProbabilityBasedOnGoodToday = BigDecimal.valueOf(0);
+    private BigDecimal goodInvestmentProbabilityBasedOnRiskyToday = BigDecimal.valueOf(0);
+    private BigDecimal goodInvestmentTotalProbabilityBasedOnToday = BigDecimal.valueOf(0);
+    private Map<String, StockPriceData> morningMap = new HashMap<>();
 }
